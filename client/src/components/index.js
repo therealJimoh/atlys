27025c3d749042411bf0b7ea011971fc9567b4c8
therealJimoh <jimoh.abdullah69@gmail.com>
2022-09-18 14:41:00 +0100

    removed cached files

--- client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Time } from "./time/Time"
-export { default as Card } from "./card/Card"
-export { default as Entry } from "./entry/Entry"
-export { default as AddBtn } from "./addbtn/AddBtn"
--- client/src/container/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Navbar } from "./navbar/Navbar"
-export { default as TrackCard } from "./trackcard/TrackCard"
